8370d481948ac999529ed19f0179966ddfd5459d
Ian <magictical@gmail.com>
2017-03-21 12:08:40 +0900

    implements OnmapReadyCallback in MainActivity this map will inflate the google map and show busan to user

--- app/src/main/java/com/example/android/mapappusemarker/MainActivity.java
+++ app/src/main/java/com/example/android/mapappusemarker/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.android.mapappusemarker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Busan = new LatLng(35.107244, 128.970783);
+        CameraPosition position = CameraPosition.builder().target(Busan).zoom(10).build();
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
     }
 }
