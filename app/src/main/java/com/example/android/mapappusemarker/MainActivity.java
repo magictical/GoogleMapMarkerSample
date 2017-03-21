43ec228e819efb0d8ab99c28a82d9adf6b2c3a9d
Ian <magictical@gmail.com>
2017-03-21 18:11:29 +0900

    Add Marker in the map feature there is 6 markers in the S.Korea's map

--- app/src/main/java/com/example/android/mapappusemarker/MainActivity.java
+++ app/src/main/java/com/example/android/mapappusemarker/MainActivity.java
@@ -9,15 +9,67 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Map;
 
+import static android.R.attr.fragment;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
     GoogleMap map;
+     static final LatLng home = new LatLng(35.107291, 128.970803);
+
+    //map ready instance
+    private boolean onMapReady = false;
+
+    //MarkerOptions 인스턴스 6개 생성
+    private MarkerOptions busan;
+    private MarkerOptions seoul;
+    private MarkerOptions jeju;
+    private MarkerOptions dangridong;
+    private MarkerOptions haundae;
+    private MarkerOptions gujedo;
+
+    //CameraPosition클래스의 Seatle 상수로 선언하기
+    static final CameraPosition HOME = CameraPosition.builder()
+            .target(home)
+            .zoom(10)
+            .bearing(90)
+            .tilt(30)
+            .build();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        busan = new MarkerOptions()
+                .position(new LatLng(35.1643694,128.9317158))
+                .title("Busan");
+
+        seoul = new MarkerOptions()
+                .position(new LatLng(37.5650172,126.8494661))
+                .title("Seoul");
+
+        jeju = new MarkerOptions()
+                .position(new LatLng(33.5038451,126.4949805))
+                .title("Jeju");
+
+        dangridong = new MarkerOptions()
+                .position(new LatLng(35.1107504,128.9664644))
+                .title("Dangridong");
+
+        haundae = new MarkerOptions()
+                .position(new LatLng(35.1769655,129.1035594))
+                .title("Haundae");
+
+        gujedo = new MarkerOptions()
+                .position(new LatLng(34.8869142,128.6216973))
+                .title("Gujedo");
+
+
+
 
         MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(this);
@@ -25,9 +77,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        onMapReady = true;
         map = googleMap;
-        LatLng Busan = new LatLng(35.107244, 128.970783);
-        CameraPosition position = CameraPosition.builder().target(Busan).zoom(10).build();
+        map.addMarker(busan);
+        map.addMarker(seoul);
+        map.addMarker(jeju);
+        map.addMarker(dangridong);
+        map.addMarker(haundae);
+        map.addMarker(gujedo);
+
+        flyToHome(HOME);
+    }
+
+    private void flyToHome(CameraPosition position) {
         map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
     }
 }
